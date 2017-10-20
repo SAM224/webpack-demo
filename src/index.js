3befcaae2efd68e39339ac035d764a0a6836d8cc
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-10-20 17:02:46 +0200

    ** source map in prod. ** Specify enviroments.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import Data from './data.xml';
 import printMe from './print.js';
 
 function component() {
-  
+
   var element = document.createElement('div');
   var btn = document.createElement('button');
-  
+
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
+
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
   element.appendChild(btn);
